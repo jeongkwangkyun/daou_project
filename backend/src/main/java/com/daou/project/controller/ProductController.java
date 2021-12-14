c408add13050eb6a639d26bd2b2358417b1b5cdc
jeongkwangkyun <rhkdrbs06@naver.com>
2021-12-15 08:58:47 +0900

    Refactor : 롬복 추가 및 카멜 표기법 수정

--- backend/src/main/java/com/daou/project/controller/ProductController.java
+++ backend/src/main/java/com/daou/project/controller/ProductController.java
@@ -4,7 +4,6 @@ import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,15 +13,17 @@ import org.springframework.web.bind.annotation.RestController;
 import com.daou.project.model.ProductDto;
 import com.daou.project.model.service.ProductService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/product")
 public class ProductController {
 	private static final Logger logger = LoggerFactory.getLogger(ProductController.class);
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
 	
-	@Autowired
-	private ProductService productService;
+	private final ProductService productService;
 	
 	@GetMapping
 	public ResponseEntity<List<ProductDto>> listProduct() throws Exception{
--- backend/src/main/java/com/daou/project/model/ProductDto.java
+++ backend/src/main/java/com/daou/project/model/ProductDto.java
@@ -1,26 +1,12 @@
 package com.daou.project.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ProductDto {
-	private int prd_no;
-	private String prd_name;
-	private int prd_price;
-	public int getPrd_no() {
-		return prd_no;
-	}
-	public void setPrd_no(int prd_no) {
-		this.prd_no = prd_no;
-	}
-	public String getPrd_name() {
-		return prd_name;
-	}
-	public void setPrd_name(String prd_name) {
-		this.prd_name = prd_name;
-	}
-	public int getPrd_price() {
-		return prd_price;
-	}
-	public void setPrd_price(int prd_price) {
-		this.prd_price = prd_price;
-	}
-	
+	private int productNo;
+	private String productName;
+	private int productPrice;
 }
