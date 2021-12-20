6fa917c8d6887ef08de955b22ca602993f57cc22
jeongkwangkyun <rhkdrbs06@naver.com>
2021-12-21 01:02:59 +0900

    Feat : exception 처리

--- backend/src/main/java/com/daou/project/controller/PaymentController.java
+++ backend/src/main/java/com/daou/project/controller/PaymentController.java
@@ -1,9 +1,13 @@
 package com.daou.project.controller;
 
+
+import javax.validation.Valid;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +39,7 @@ public class PaymentController {
 	}
 	
 	@PostMapping("/direct")
-	public ResponseEntity<String> registerPayment(@RequestBody RequestPaymentDto reqPaymentDto) throws Exception{
+	public ResponseEntity<String> registerPayment(@Valid @RequestBody RequestPaymentDto reqPaymentDto) throws Exception{
 		if(paymentService.registerPayment(reqPaymentDto)) {
 			return new ResponseEntity<String>(SUCCESS, HttpStatus.OK);
 		}
